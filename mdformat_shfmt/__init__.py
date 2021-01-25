1c15ff2330d49103dbfcc76ebf473671e7d98b94
Taneli Hukkinen <hukkinj1@users.noreply.github.com>
2021-01-26 00:44:38 +0200

    Bump version: 0.0.0 → 0.1.0

--- mdformat_shfmt/__init__.py
+++ mdformat_shfmt/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
+__version__ = "0.1.0"  # DO NOT EDIT THIS LINE MANUALLY. LET bump2version UTILITY DO IT
 
 import subprocess
 
